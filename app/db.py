08d377b93effcb35a62175df02871fab730b9a82
Lorenzo Peve <lpeve01@gmail.com>
2024-06-23 18:35:54 -0500

    init CRUD ops

--- app/db.py
+++ app/db.py
@@ -15,29 +15,27 @@ conn_string = (
     f"password={os.getenv('DB_PASSWORD')}"
 )
 
-pool = ConnectionPool(
+POOL = ConnectionPool(
     conninfo=conn_string,
     open=True,
     min_size=3
     )
 
-def create_connection() -> psycopg.connection:
-    try:
-        conn = psycopg.connect(
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-        )
-        print("Connection to PostgreSQL DB successful")
-        return conn
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-        raise e
+def set_inputs_to_none_if_empty(func):
+    """Decorator that sets empty strings to None in function arguments."""
+    def wrapper(*args, **kwargs):
+
+        modified_args = tuple(None if arg == '' else arg for arg in args)
+        modified_kwargs = {key: (None if value == '' else value) for key, value in kwargs.items()}
+        result = func(*modified_args, **modified_kwargs)
+        
+        return result
+    return wrapper
     
-def get_exercises(username: str) -> list[str]:
-    with pool.connection() as conn:
+def get_list_of_exercises(username: str) -> list[str]:
+    """Return a list of exercises for a given user."""
+
+    with POOL.connection() as conn:
         cur = conn.cursor()
         cur.execute(
             """
@@ -45,7 +43,78 @@ def get_exercises(username: str) -> list[str]:
             WHERE user_id = %s
             """, (username,)               
         )
-        exercises = cur.fetchall()
 
-    return exercises
+    return [s[0] for s in sorted(cur.fetchall())]
 
+@set_inputs_to_none_if_empty
+def add_exercise(
+    user_id: str,
+    exercise: str,
+    weight: float,
+    reps: int,
+    date: str,
+    after_wod: bool,
+    comment: str
+) -> None:
+    
+    """Add an exercise to the database for a given user."""
+
+    assert float(weight)
+    assert int(reps)
+
+    with POOL.connection() as conn:
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+            INSERT INTO weightlifting
+            (user_id, exercise, weight, reps, date, after_wod, comment)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """, (user_id, exercise, weight, reps, date, after_wod, comment)
+        )
+
+def delete_exercise(ids: list[int]) -> None:
+    """Delete an exercise from the database."""
+
+    s = ''
+    for i in ids:
+        s+= str(i) + ', '
+
+    query = f"DELETE FROM weightlifting WHERE id in ({s[:-2]})"
+
+    with POOL.connection() as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+
+def get_weightlifting_records(
+        username: str,
+        exercise: str
+    ) -> list[dict]:
+
+    """Queries the database for weightlifting records for a given user and exercise."""
+
+    with POOL.connection() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT id, user_id, exercise, weight, reps, date, after_wod, comment
+            FROM weightlifting
+            WHERE user_id = %s
+            AND exercise = %s
+            ORDER BY date DESC
+            """, (username, exercise)               
+        )
+
+    out = []
+    for record in cur:
+        out.append({
+            'id': record[0],
+            'user_id': record[1],
+            'exercise': record[2],
+            'weight': record[3],
+            'reps': record[4],
+            'date': record[5],
+            'after_wod': record[6],
+            'comment': record[7]
+        })
+    return out
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,71 @@
+from datetime import datetime, timedelta
+import random
+
 from app import db
 
+TEST_USERNAME = 'lorenpeve10'
+
+def _get_number_of_records(username: str) -> int:
+    with db.POOL.connection() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT COUnT(*) FROM weightlifting
+            WHERE user_id = (%s)
+            """, (username,)
+        )
+        count = cur.fetchone()[0]
+    return count
 
 
 def test_get_exercises():
+    exercises = db.get_list_of_exercises(TEST_USERNAME)    
+    assert 'deadlift' in exercises
+    
+def test_add_exercise():
 
-    exercises = db.get_exercises('lorenpeve10')
-    print(exercises)
+    # Before adding an exercise
+    exercise_name = 'test12345'
+    exercises = db.get_list_of_exercises(TEST_USERNAME)
+    assert exercise_name not in exercises
+
+    # Add an exercise
+    db.add_exercise(
+        user_id=TEST_USERNAME,
+        exercise=exercise_name,
+        weight=100,
+        reps=10,
+        date=datetime.now().date() + timedelta(days=1),
+        after_wod=False,
+        comment='test comment'
+    )
+
+    exercises = db.get_list_of_exercises(TEST_USERNAME)
+    assert exercise_name in exercises
+
+    # Delete newly inserted exercise
+    exercises = db.get_weightlifting_records(TEST_USERNAME, exercise_name)
+    id_to_delete = exercises[0]['id']
+    db.delete_exercise([id_to_delete])
+
+def test_delete_exercise():
+
+    exercises = db.get_list_of_exercises(TEST_USERNAME)
+    name = random.choice(exercises)
+
+    n_old = _get_number_of_records(TEST_USERNAME)
+    assert n_old > 0
+    
+    exercises = db.get_weightlifting_records(TEST_USERNAME, name)
+    ids = [exercise['id'] for exercise in exercises[:3]]
+
+    db.delete_exercise(ids)
+    
+    n_new = _get_number_of_records(TEST_USERNAME)
+    assert n_new == n_old - len(ids)
+
+def test_get_weightlifting_records():
+
+    records = db.get_weightlifting_records(TEST_USERNAME, 'deadlift')
+    assert len(records) > 0
+    assert records[0]['exercise'] == 'deadlift'
