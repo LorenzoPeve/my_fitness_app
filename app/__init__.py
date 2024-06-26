879efb7ee69c921b3280a348e41c86d94576172d
Lorenzo Peve <lpeve01@gmail.com>
2024-06-26 18:41:50 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
 from flask import Flask
+import os
+load_dotenv(override=True)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY'), 'utf-8'
 
 from app import routes
