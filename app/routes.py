27ca77b652a881989c0356320fb59b9ff39bf56f
Lorenzo Peve <lpeve01@gmail.com>
2024-06-30 16:10:28 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from functools import wraps
 from flask import render_template, request, session, make_response
-from flask import redirect, url_for
+from flask import redirect, url_for, jsonify
 import os
 import secrets
 
@@ -19,7 +20,7 @@ def auth_required(f):
 def get_csrf_token():
     return secrets.token_urlsafe(64)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -71,6 +72,7 @@ def login():
                 invalid_message="Incorrect password. Please try again."
             )
     else:
+        session.clear()
         session['username'] = username
         session['csrf_token'] = get_csrf_token()
 
@@ -78,7 +80,7 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
+    session.clear()
     return redirect(url_for('login'))
 
 @app.route('/my_records', methods=['GET', 'POST'])
@@ -94,6 +96,7 @@ def my_records():
             session['my_records_exercise'],
             session['my_records_reps']
         )
+
         return render_template("records.html", records=records)
     
     if request.form.get('csrf_token') != session['csrf_token']:
@@ -122,3 +125,47 @@ def delete_records():
     db.delete_exercise(ids, session['username'])
  
     return redirect(url_for('my_records'))
+
+@app.route('/add_records', methods=['GET', 'POST'])
+@auth_required
+def add_records():
+    if request.method == "GET":
+        return render_template("add_records.html")
+    
+    if request.form.get('csrf_token') != session['csrf_token']:
+        return make_response("Invalid request. CSRF token authentication failed.", 400)
+    
+    if request.form.get('exercise_type') == 'weight_reps':
+        kwargs = {
+            "user_id": session['username'],
+            "exercise": request.form.get('exercise'),
+            "weight": request.form.get('weight'),
+            "reps": request.form.get('n_reps'),
+            "date": request.form.get('date'),
+            "after_wod" : request.form.get('is_after_wod', False),
+            "comment": request.form.get('activity_comments')
+        }
+
+        n_sets = int(request.form.get('n_sets'))
+        if n_sets == 1:
+            db.add_exercise(**kwargs)
+        else:
+            db.add_exercise_multiple_times(n_sets, **kwargs)
+
+    session['my_records_exercise'] = request.form.get('exercise')
+    session['my_records_reps'] = request.form.get('n_reps')
+    return redirect(url_for('my_records'))
+
+@app.route('/get_records', methods=['POST'])
+@auth_required
+def get_records():
+
+    if request.form.get('csrf_token') != session['csrf_token']:
+        return make_response(
+            jsonify({"error": "Invalid request. CSRF token authentication failed."}),
+            400,
+            {'Content-Type': 'application/json'}
+        )
+    print(datetime.now())
+    records = db.get_list_of_exercises(session['username'])
+    return jsonify(records)
